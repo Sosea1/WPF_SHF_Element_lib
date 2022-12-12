4952bfb5c47b40028ec15ab862fba56f12b0d9d3
SealCHK <kospryshin@gmail.com>
2022-12-13 00:51:28 +0600

    Добавьте файлы проекта.

---
+++ WPF_SHF_Element_lib/Window1.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Path = System.IO.Path;
+
+namespace WPF_SHF_Element_lib
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        Dictionary<string, string> values = new Dictionary<string, string>();
+        string group, name;
+        string[] parameters;
+        string file;
+        string temp_values;
+        Window2 window2 = new Window2();
+        Window3 window3 = new Window3();
+        List<Element> elements;
+        public Window1()
+        {
+
+            InitializeComponent();
+        }
+
+
+
+
+        public void copy_file()
+        {
+            File.Copy(file, AppDomain.CurrentDomain.BaseDirectory + @"\element_picture\" + Path.GetFileName(nameElement.Text) + ".png");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            this.Hide();
+
+            window2.Show();
+
+
+
+        }
+
+        private void Label1_Next_Click(object sender, EventArgs e)
+        {
+            if (values_textbox.Text != temp_values)
+            {
+                // dataGrid1_Fill();
+            }
+            temp_values = values_textbox.Text;
+            group = comboBox1.SelectedItem.ToString();
+            name = nameElement.Text;
+            parameters = params_textbox.Text.Split(',');
+            //string[] values = textBox2.Text.Split(',');
+            //int i = 0;
+            /* foreach (string value in values)
+             {
+                 dataGridView1.Rows.Add();
+                 dataGridView1.Rows[i].HeaderCell.Value = value;
+                 i++;
+             }*/
+
+        }
+
+
+        public void dataGrid1_Fill()
+        {
+            elements = values_textbox.Text.Split(',').ToList;
+        }
+        public List<Element> dataElements()
+        {
+            return elements;
+        }
+    }
+
+}
---
+++ WPF_SHF_Element_lib/Window2.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Navigation;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF_SHF_Element_lib
+{
+    /// <summary>
+    /// Логика взаимодействия для Window2.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+        public Window2()
+        {
+            InitializeComponent();
+           
+            grid.ItemsSource = elements;
+        }
+
+       List<Element> elements = new List<Element>();
+        
+    }
+    
+
+   public class Element
+    {
+        public Element(string firstName, string lastName)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            
+        }
+
+        public string FirstName { get { return this.firstName; } set { this.firstName = value; } }
+        public string LastName { get { return this.lastName; } set { this.lastName = value; } }
+ 
+
+        private string firstName;
+        private string lastName;
+       
+
+    }
+
+
+
+
+}
